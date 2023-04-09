25f2befc3c4663ad819066ed292176416b3040f3
Lisa-Krejci <125563976+Lisa-Krejci@users.noreply.github.com>
2023-04-09 11:58:55 +0200

    Add files via upload
    Odstraněné duplicity, přidána kontrola ValueError.

--- content-calculation.py
+++ content-calculation.py
@@ -12,30 +12,25 @@ def obsah_elipsy(a, b):
     obsah = pi * a * b
     return obsah
 
-def pokracovat():
+def pokracovat():  # Výběr, zda chce uživatel pokračovat nebo práci ukončit
     while True:
         pokracovat = input('Přeješ si vypočítat něco dalšího? (odpověz "ano" nebo "ne") ')
         if pokracovat  == 'ano':
-            tvar = input('''
-Pro který tvar chceš vypočítat obsah?
-1. Čtverec
-2. Obdélník
-3. Kruh
-4. Elipsa
-
-Zadej: ''')
-            break
+            tvar = 0
+            return (tvar)
             
         elif pokracovat == 'ne':
             tvar = 'ne'
-            break
+            return (tvar)
 
         else:
             print('\nOdpověz prosím pouze "ano" nebo "ne".')
-    return (tvar)
 
+tvar = 0
 
-tvar = input('''
+while tvar != 'ne':
+
+    tvar = input('''
 Pro který tvar chceš vypočítat obsah?
 1. Čtverec
 2. Obdélník
@@ -44,43 +39,53 @@ Pro který tvar chceš vypočítat obsah?
 
 Zadej: ''')
 
-while True:
     if tvar == '1' or tvar == '1.' or tvar == 'Čtverec' or tvar == 'čtverec' or tvar == '1. Čtverec' or tvar == '1. čtverec':
-        c = int(input('Zadej délku strany: '))
-        print('Obsah čtverce je: ', obsah_obdelnika(c, c), '.', sep='', end='\n\n')
         
-        tvar = pokracovat()
+        while True:
+            try:
+                c = int(input('Zadej délku strany: '))
+                print('Obsah čtverce je: ', obsah_obdelnika(c, c), '.', sep='', end='\n\n')       
+                tvar = pokracovat()
+                break
+            except ValueError: 
+                print('\nZadej prosím kladné celé číslo.')
 
     elif tvar == '2' or tvar == '2.' or tvar == 'Obdélník' or tvar == 'obdélník' or tvar == '2. Obdélník' or tvar == '2. obdélník':
-        c = int(input('Zadej délku první strany: '))
-        d = int(input('Zadej délku druhé strany: '))
-        print ('Obsah obdélníka je: ', obsah_obdelnika(c, d), '.', sep = '', end='\n\n')
 
-        tvar = pokracovat()
+            while True:
+                try:
+                    c = int(input('Zadej délku první strany: '))
+                    d = int(input('Zadej délku druhé strany: '))
+                    print ('Obsah obdélníka je: ', obsah_obdelnika(c, d), '.', sep = '', end='\n\n')
+                    tvar = pokracovat()
+                    break
+                except ValueError: 
+                    print('\nZadej prosím kladná celá čísla.')
 
     elif tvar == '3' or tvar == '3.' or tvar == 'Kruh' or tvar == 'kruh' or tvar == '3. Kruh' or tvar == '3. kruh':
-        r = int(input('Zadej délku poloměru: '))
-        print('Obsah kruhu je: ', obsah_kruhu(r), '.', sep = '', end='\n\n')
-
-        tvar = pokracovat()
+                
+        while True:
+            try:
+                r = int(input('Zadej délku poloměru: '))
+                print('Obsah kruhu je: ', obsah_kruhu(r), '.', sep = '', end='\n\n')    
+                tvar = pokracovat()
+                break
+            except ValueError: 
+                print('\nZadej prosím kladné celé číslo.')
 
     elif tvar == '4' or tvar == '4.' or tvar == 'Elipsa' or tvar == 'elipsa' or tvar == '4. Elipsa' or tvar == '4. elipsa':
-        a = int(input('Zadej délku první poloosy: '))
-        b = int(input('Zadej délku druhé poloosy: '))
-        print('Obsah obdélníka je: ', obsah_elipsy(a, b), '.', sep = '', end='\n\n')
-
-        tvar = pokracovat()
-    
-    elif tvar == 'ne':
-        print('Dobrá, tak zase příště nashledanou.')
-        break
+        
+            while True:
+                try:
+                    a = int(input('Zadej délku první poloosy: '))
+                    b = int(input('Zadej délku druhé poloosy: '))
+                    print('Obsah elipsy je: ', obsah_elipsy(a, b), '.', sep = '', end='\n\n')
+                    tvar = pokracovat()
+                    break
+                except ValueError: 
+                    print('\nZadej prosím kladná celá čísla.')
 
     else:
-        tvar = input('''
-Pro který tvar chceš vypočítat obsah?
-1. Čtverec
-2. Obdélník
-3. Kruh
-4. Elipsa
-    
-Zadej: ''')
+        print('\nVyber prosím jeden z uvedených tvarů.')
+
+print('Dobrá, tak zase příště nashledanou.')
